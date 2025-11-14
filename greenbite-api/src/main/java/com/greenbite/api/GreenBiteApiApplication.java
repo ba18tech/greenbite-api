f479f0db6f3c3b8b3ba66b76736439e1375f4ad3
00Bhavani <bhavanigowda0906@gmail.com>
2025-11-14 05:42:11 +0530

    GreenBite initial commit

---
+++ greenbite-api/src/main/java/com/greenbite/api/GreenBiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenbite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBiteApiApplication.class, args);
+	}
+
+}
---
+++ greenbite-api/src/test/java/com/greenbite/api/GreenbiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbite.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
